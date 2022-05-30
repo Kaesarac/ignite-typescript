9c8c515b318be3d7662ece428b700765b5b5e037
Augusto Cesar <augustocesar19silva@gmail.com>
2022-05-30 11:38:17 -0300

    added promise logic to importCategoryUseCase

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,7 +1,42 @@
+import fs from 'fs';
+import { parse } from 'csv-parse';
+import { ICategoriesRepository } from '../../repositories/implementations/ICategoriesRepository';
+
+interface IImportCategory {
+    name: string;
+    description: string;
+}
 
 class ImportCategoryUseCase {
-    execute(file: any){
-        console.log(file);
+    constructor(private categoriesRepository: ICategoriesRepository){}
+
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]>{
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path);
+            const categories: IImportCategory[] = [];
+            const parsefile = parse({});
+
+            stream.pipe(parsefile);
+            
+            parsefile.on('data', async (line) =>{
+                const [ name, description ] = line;
+                categories.push({
+                    name, 
+                    description,
+                });
+            });
+            parsefile.on('end', () => {
+                resolve(categories);
+            });
+            parsefile.on('error', (err) => {
+                reject(err);
+            });
+            })
+    }
+
+    async execute(file: Express.Multer.File): Promise<void>{
+        const categories = await this.loadCategories(file);
+        console.log(categories);
         
     }
 }
